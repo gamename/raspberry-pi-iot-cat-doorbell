56f96e66065a7ac54690dc9ce63e0efcff449f72
Tennis Smith <tennis.n.smith@gmail.com>
2022-08-03 10:17:35 -0500

    take host path off of dest url

--- raspberry_pi/start-doorbell.sh
+++ raspberry_pi/start-doorbell.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python3 ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/doorbell.py \
-  --endpoint ${HOME}/a3u37c52vq0b6j-ats.iot.us-east-1.amazonaws.com \
+  --endpoint a3u37c52vq0b6j-ats.iot.us-east-1.amazonaws.com \
   --rootCA ${HOME}/root-CA.crt \
   --cert ${HOME}/cat-doorbell.cert.pem \
   --key ${HOME}/cat-doorbell.private.key \
