c5044efa2694ac5cfecbaf9bd74d7e52b140b1c6
Tennis Smith <tennis.n.smith@gmail.com>
2022-08-03 10:02:19 -0500

    add startup script

---
+++ raspberry_pi/start-doorbell.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+python3 ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/doorbell.py \
+  --endpoint ${HOME}/a3u37c52vq0b6j-ats.iot.us-east-1.amazonaws.com \
+  --rootCA ${HOME}/root-CA.crt \
+  --cert ${HOME}/cat-doorbell.cert.pem \
+  --key ${HOME}/cat-doorbell.private.key \
+  --model ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/yamnet.tflite
