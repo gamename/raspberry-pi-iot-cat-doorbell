1d24c8d3c97bf7ac0ded7ae3490dd53f73fd5853
Tennis Smith <tennis.n.smith@gmail.com>
2022-08-04 04:33:37 -0500

    add log redirection

--- raspberry_pi/start-doorbell.sh
+++ raspberry_pi/start-doorbell.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-python3 ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/doorbell.py \
+nohup python3 ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/doorbell.py \
   --endpoint a3u37c52vq0b6j-ats.iot.us-east-1.amazonaws.com \
   --rootCA ${HOME}/root-CA.crt \
   --cert ${HOME}/cat-doorbell.cert.pem \
   --key ${HOME}/cat-doorbell.private.key \
-  --model ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/yamnet.tflite
+  --model ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/yamnet.tflite > /tmp/doorbell.log 2>&1 &
