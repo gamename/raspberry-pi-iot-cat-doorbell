9326f7ec700d55470c69774b97c9102bac342a70
gamename <tennis.n.smith@gmail.com>
2022-08-04 04:42:52 -0500

    remove unused model parm

--- raspberry_pi/drive-doorbell-test.sh
+++ raspberry_pi/drive-doorbell-test.sh
@@ -3,5 +3,4 @@ python3 ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/doorbell-test.py \
   --endpoint a3u37c52vq0b6j-ats.iot.us-east-1.amazonaws.com \
   --rootCA ${HOME}/root-CA.crt \
   --cert ${HOME}/cat-doorbell.cert.pem \
-  --key ${HOME}/cat-doorbell.private.key \
-  --model ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/yamnet.tflite
+  --key ${HOME}/cat-doorbell.private.key
