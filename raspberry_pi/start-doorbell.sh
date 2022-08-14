3b0195a644902bf2037f7aa33ceb202292947617
gamename <tennis.n.smith@gmail.com>
2022-08-14 13:16:16 -0500

    lint changes and flags to update fields

--- raspberry_pi/start-doorbell.sh
+++ raspberry_pi/start-doorbell.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-nohup python3 ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/doorbell.py \
+nohup python3 "${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/doorbell.py" \
   --endpoint a3u37c52vq0b6j-ats.iot.us-east-1.amazonaws.com \
-  --rootCA ${HOME}/root-CA.crt \
-  --cert ${HOME}/cat-doorbell.cert.pem \
-  --key ${HOME}/cat-doorbell.private.key \
-  --topic "tns/bot/cat-doorbell" \
+  --rootCA "${HOME}/root-CA.crt" \
+  --cert "${HOME}/cat-doorbell.cert.pem" \
+  --key "${HOME}/cat-doorbell.private.key" \
+  --topic "<your-initials>/bot/cat-doorbell" \
   --message "MSG002 Milo at the door!" \
-  --model ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/yamnet.tflite > /tmp/doorbell.log 2>&1 &
+  --model "${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/yamnet.tflite" > /tmp/doorbell.log 2>&1 &
