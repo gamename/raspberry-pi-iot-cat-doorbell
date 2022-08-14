7e98dcd6a253be755b271e5d9cf2210df4488f51
gamename <tennis.n.smith@gmail.com>
2022-08-14 13:01:20 -0500

    update to make clearer

--- raspberry_pi/test/drive-doorbell-test.sh
+++ raspberry_pi/test/drive-doorbell-test.sh
@@ -3,4 +3,7 @@ python3 ${HOME}/raspberry-pi-iot-cat-doorbell/raspberry_pi/test/doorbell-test.py
   --endpoint a3u37c52vq0b6j-ats.iot.us-east-1.amazonaws.com \
   --rootCA ${HOME}/root-CA.crt \
   --cert ${HOME}/cat-doorbell.cert.pem \
-  --key ${HOME}/cat-doorbell.private.key
+  --key ${HOME}/cat-doorbell.private.key \
+  --topic "<your-initials>/bot/cat-doorbell" \
+  --message "MSG002 Cat at the door!" \
+
